e42ef048799765d80d78d1eb4feccc5cbd199097
zxreep <shortsmaker819@gmail.com>
2026-02-01 20:13:03 +0530

    Enhance Player component with fullscreen support
    Added forceFullscreen prop to enable fullscreen mode on channel select. Improved video handling with additional Hls.js configuration.

--- components/Player.js
+++ components/Player.js
@@ -1,26 +1,57 @@
 import { useEffect, useRef } from 'react'
 import Hls from 'hls.js'
 
-export default function Player({ url }) {
-  const ref = useRef(null)
+export default function Player({ url, forceFullscreen = false }) {
+  const videoRef = useRef(null)
 
   useEffect(() => {
-    const video = ref.current
+    const video = videoRef.current
     if (!video) return
 
+    // Force fullscreen on channel select
+    if (forceFullscreen && video.requestFullscreen) {
+      setTimeout(() => {
+        video.requestFullscreen().catch(() => {})
+      }, 300)
+    }
+
+    // Safari native HLS
     if (video.canPlayType('application/vnd.apple.mpegurl')) {
       video.src = url
-      video.play()
+      video.play().catch(() => {})
       return
     }
 
+    // hls.js for other browsers
     if (Hls.isSupported()) {
-      const hls = new Hls()
+      const hls = new Hls({
+        lowLatencyMode: true,
+        backBufferLength: 90,
+        maxBufferLength: 60,
+        maxMaxBufferLength: 120,
+        liveSyncDuration: 3,
+        liveMaxLatencyDuration: 10,
+        enableWorker: true
+      })
+
       hls.loadSource(url)
       hls.attachMedia(video)
+
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        video.play().catch(() => {})
+      })
+
       return () => hls.destroy()
     }
-  }, [url])
+  }, [url, forceFullscreen])
 
-  return <video ref={ref} controls className="w-full h-full bg-black" />
-    }
+  return (
+    <video
+      ref={videoRef}
+      controls
+      autoPlay
+      playsInline
+      className="w-full h-full object-contain bg-black"
+    />
+  )
+        }
